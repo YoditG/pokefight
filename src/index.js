c9bf3ed9dae0c80978e80ee561b6a7a5c6a29680
YoditG <y.geb@web.de>
2020-11-03 18:21:03 +0100

    add router controller mockdata

--- src/index.js
+++ src/index.js
@@ -1,7 +1,20 @@
 const express = require('express')
 const app = express();
 
-const cors = require('cors')
+const cors = require('cors');
+const pokeRouter = require('./routes/pokeRouter');
 
 app.use(cors())
+app.use('/pokemon',pokeRouter)
 
+
+
+
+const port = process.env.port || 3000;
+
+
+
+
+app.listen(port, ()=>{
+    console.log(`Server is listening on port ${port}`);
+})
