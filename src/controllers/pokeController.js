93319e160d94b10c56d8442613673cf3137e9d55
Makuku-888 <koch.marcus01@gmail.com>
2020-11-04 18:10:27 +0100

    added middelware pokeCheck

--- src/controllers/pokeController.js
+++ src/controllers/pokeController.js
@@ -1,9 +1,12 @@
-const mockdata = require('../static/mockdata')
+const mockdata = require("../static/mockdata");
 const poketroller = {
-    getAllPoke: (req,res)=>{
-        res.send(JSON.stringify(mockdata))
-    }
-
-}
+  getAllPoke: (req, res) => {
+    res.send(JSON.stringify(mockdata));
+  },
+  getOnePoke: (req, res) => {
+    
+    res.send(req.currentPokemon);
+  },
+};
 
 module.exports = poketroller;
---
+++ src/middlewares/pokeCheck.js
@@ -0,0 +1,16 @@
+const mockdata = require('../static/mockdata')
+
+const pokeCheck = {
+    pokeValidation:(req, res,next) => {
+        const { id } = req.params;
+        
+        const pokemon = mockdata.filter((el) => el.id === parseInt(id,10));
+        if (!pokemon.length) {
+            return res.sendStatus(404)
+        }
+        req.currentPokemon = JSON.stringify(pokemon);
+        next();
+      }
+}
+
+module.exports = pokeCheck;
