6a75d906e79875319329a3e13878789111ceaac4
YoditG <y.geb@web.de>
2020-11-05 16:47:59 +0100

    deploy on heroku

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express();
 
 const cors = require('cors');
-const pokeRouter = require('./routes/pokeRouter');
+const pokeRouter = require('./src/routes/pokeRouter');
 
 app.use(cors());
 app.use(express.json());
